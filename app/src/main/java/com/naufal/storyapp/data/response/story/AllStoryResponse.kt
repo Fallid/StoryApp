07e714e7eecf8456b5399f641bd9d48809447769
Fallid <naufalopal002@gmail.com>
2023-11-14 16:40:28 +0700

    docs(response story): change null able list

--- app/src/main/java/com/naufal/storyapp/data/response/story/AllStoryResponse.kt
+++ app/src/main/java/com/naufal/storyapp/data/response/story/AllStoryResponse.kt
@@ -8,7 +8,7 @@ import com.google.gson.annotations.SerializedName
 data class AllStoryResponse(
 
 	@field:SerializedName("listStory")
-	val listStory: List<ListStoryItem?>? = null,
+	val listStory: List<ListStoryItem>,
 
 	@field:SerializedName("error")
 	val error: Boolean? = null,
